7a06d5e81800103e64dba6359de2d82bef0bfb37
Vinicius de Souza Santos <viniciuskt0@gmail.com>
2020-08-07 23:46:17 -0300

    Teacher Form

--- web/src/pages/TeacherForm/index.tsx
+++ web/src/pages/TeacherForm/index.tsx
@@ -1,10 +1,85 @@
 import React from 'react';
 import PageHeader from '../../components/PageHeader';
+import Input from '../../components/Input';
+import Textarea from '../../components/Textarea';
+import Select from '../../components/Select';
+
+import './styles.css';
+
+import warningIcon from '../../assets/images/icons/warning.svg';
+
 
 const TeacherForm: React.FC = () => {
   return (
     <div id="page-teacher-form" className="container">
-      <PageHeader title="Que incrível que você quer dar aulas."/>
+      <PageHeader 
+        title="Que incrível que você quer dar aulas."
+        description="O primeiro é preencher esse formulário de inscrição"
+      />
+
+      <main>
+        <fieldset>
+          <legend>Seus dados</legend>
+
+          <Input name="name" label="Nome completo"/>
+          <Input name="avatar" label="Avatar"/>
+          <Input name="whatsapp" label="Whatsapp"/>
+
+          <Textarea name="bio" label="Biografia"/>
+        </fieldset>
+
+        <fieldset>
+          <legend>Sobre a aula</legend>
+
+          <Select 
+            name="subject" 
+            label="Matéria"
+            options={[
+              { value: 'Artes', label: 'Artes'},
+              { value: 'Biologia', label: 'Biologia'},
+              { value: 'Ciências', label: 'Ciências'},
+              { value: 'Geografia', label: 'Geografia'},
+              { value: 'Matemática', label: 'Matemática'},
+            ]}
+          />
+          <Input name="cost" label="Custo da sua hora por aula"/>
+        </fieldset>
+
+        <fieldset>
+          <legend>
+            Horários disponíveis <button type="button"> + Novo Horário</button>
+          </legend>
+
+          <div className="schedule-item">
+            <Select 
+              name="week_day" 
+              label="Dia da semana"
+              options={[
+                { value: '0', label: 'Domingo'},
+                { value: '1', label: 'Segunda'},
+                { value: '2', label: 'Terça'},
+                { value: '3', label: 'Quarta'},
+                { value: '4', label: 'Quinta'},
+                { value: '5', label: 'Sexta'},
+                { value: '6', label: 'Sábado'},
+              ]}
+            />
+            <Input name="from" label="Das" type="time"/>
+            <Input name="to" label="Até" type="time"/>
+          </div>
+        </fieldset>
+
+        <footer>
+          <p>
+            <img src={warningIcon} alt="Aviso importante"/>
+            Importante! <br/>
+            Preencha todos os dados
+          </p>
+          <button type="button">
+            Salvar o cadastro
+          </button>
+        </footer>
+      </main>
     </div>
   );
 }
